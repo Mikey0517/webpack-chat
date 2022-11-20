d8d3752ed0647dc030dcb983bb479cd4cb918e1c
Mikey <491444588@qq.com>
2022-11-20 14:37:35 +0800

    fix: socket.io

--- src/socket/index.ts
+++ src/socket/index.ts
@@ -2,12 +2,15 @@
  * @Author       : 徐洋皓月
  * @Date         : 2022-11-14 17:19:54
  * @LastEditors  : 徐洋皓月
- * @LastEditTime : 2022-11-20 03:21:43
+ * @LastEditTime : 2022-11-20 14:36:47
  * @FilePath     : /chat-webpack/src/socket/index.ts
  */
 import { io } from "socket.io-client";
 
-// @ts-ignore
-const socket: any = io(PUBLIC_PATH, { autoConnect: false });
+const socket: any = io('/', { 
+  autoConnect: false,
+  // @ts-ignore
+  path: `${PUBLIC_PATH}/socket.io/`
+});
 
 export default socket;
